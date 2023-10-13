0989c068935c7330ce898ed99e05e29b835094a6
WALLISONALCANTARA <wallisonandrew55@hotmail.com>
2023-10-13 15:22:29 -0300

    teste

---
+++ bin/projeto_inicial.dart
@@ -0,0 +1,14 @@
+void main(List<String> arguments) {
+  int idade = 20;
+  double altura = 1.91;
+  bool geek = true;
+  if(geek){
+    print('ele é nerd');
+    print(geek);
+    print('Minha idade: $idade e minha altura: $altura');
+  }
+  else{
+    print('ele não é nerd');
+  }
+
+}

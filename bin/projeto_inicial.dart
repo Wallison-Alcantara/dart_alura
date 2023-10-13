b6c7bb9fe6a3961dc936729baea5d5effd511d26
WALLISONALCANTARA <wallisonandrew55@hotmail.com>
2023-10-13 17:02:08 -0300

    Aula03

--- bin/projeto_inicial.dart
+++ bin/projeto_inicial.dart
@@ -1,17 +1,27 @@
 void main(List<String> arguments) {
   int idade = 20;
   double altura = 1.91;
-  bool geek = true;
+  bool geek = false;
   String nome = 'Wallison';
+  String apelido = 'Wall';
   if(geek){
     print('ele é nerd');
     print(geek);
     print('Meu nome: $nome, \n'
         'minha idade: $idade \n '
-        'minha altura: $altura');
+        'minha altura: $altura \n '
+        'meu apelido: $apelido');
   }
   else{
-    print('ele não é nerd');
+    List<String> listanomes = ['Wallison','Vitoria','Gabriel','Marcos','Vinicius','Alberto'];
+    print(listanomes[0]);
+    print(listanomes.length); //tamanho da lista
+
+    List<dynamic> wallison = [20,1.91,true,'Wallison','Wall'];
+    print(wallison);
+    print('Eu sou ${wallison[3]}, tenho ${wallison[0]} anos, '
+        'tenho ${wallison[1]} metros de altura. \n'
+        'Meu apelido é: ${wallison[4]} e sou geek? ${wallison[2]}');
   }
 
 }

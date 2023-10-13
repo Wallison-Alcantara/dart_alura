0d97d4bf4f8e9cd85b678d327c03e225eeb2fd89
WALLISONALCANTARA <wallisonandrew55@hotmail.com>
2023-10-13 16:04:30 -0300

    Aula02

---
+++ bin/conversao.dart
@@ -0,0 +1,29 @@
+void main(){
+  String teste= '20';
+  int.parse(teste); // aqui pegou o valor como Int
+  // porém, após a linhda de cima, ela volta a ser String
+
+  // String to int
+  var one = int.parse('1');
+  print("1.");
+  print(one);
+  print(one.runtimeType);
+
+  // String to double
+  var onePointOne = double.parse('1.1');
+  print("\n2.");
+  print(onePointOne);
+  print(onePointOne.runtimeType);
+
+  // int to String
+  var twenty = 20.toString();
+  print("\n3.");
+  print(twenty);
+  print(twenty.runtimeType);
+
+  // double to String
+  var pi = 3.14316.toStringAsFixed(2);
+  print("\n4.");
+  print(pi);
+  print(pi.runtimeType);
+}
--- bin/projeto_inicial.dart
+++ bin/projeto_inicial.dart
@@ -2,10 +2,13 @@ void main(List<String> arguments) {
   int idade = 20;
   double altura = 1.91;
   bool geek = true;
+  String nome = 'Wallison';
   if(geek){
     print('ele é nerd');
     print(geek);
-    print('Minha idade: $idade e minha altura: $altura');
+    print('Meu nome: $nome, \n'
+        'minha idade: $idade \n '
+        'minha altura: $altura');
   }
   else{
     print('ele não é nerd');
